2562730dbaca21e8e136c7ba2197922e4ee41c5d
akhiltheruvath836-ctrl <akhiltheruvath836@gmail.com>
2025-12-05 14:51:33 +0530

    add

---
+++ BrandShop/app/page.js
@@ -0,0 +1,18 @@
+// import Image from "next/image";
+// import Link from "next/link";
+
+import Homepage from "../app/homepage/page"
+
+export default function Page() {
+  return (
+    <div >
+     
+
+
+  
+   <Homepage/>
+
+
+    </div>
+  );
+}
