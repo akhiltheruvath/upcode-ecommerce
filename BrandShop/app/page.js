b4b4891107c9aefb6a903b3278d3f6437764ecf5
akhiltheruvath836-ctrl <akhiltheruvath836@gmail.com>
2025-12-05 15:18:08 +0530

    fix: add root page and correct import

--- BrandShop/app/page.js
+++ BrandShop/app/page.js
@@ -1,18 +1,11 @@
-// import Image from "next/image";
-// import Link from "next/link";
 
-import Homepage from "../app/homepage/page"
 
-export default function Home() {
+import Homepage from "../app/homepage/page"; 
+
+export default function Page() {
   return (
-    <div >
-     
-
-
-  
-   <Homepage/>
-
-
+    <div>
+      <Homepage />
     </div>
   );
 }
